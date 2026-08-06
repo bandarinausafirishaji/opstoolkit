d3ffcda8493e5461a04fea3431b799778a06753f
bandarinausafirishaji <abdallaallysaid@gmail.com>
2026-08-06 13:28:36 +0300

    Add TEU Calculator function

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("OpsToolkit is running successfully");
+
+
+function calculateTEU(){
+
+let twenty = Number(document.getElementById("twenty").value);
+
+let forty = Number(document.getElementById("forty").value);
+
+
+let totalTEU = (twenty * 1) + (forty * 2);
+
+
+document.getElementById("teuResult").innerHTML =
+"Total TEU: " + totalTEU;
+
+}
